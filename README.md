cd0040dff32b5f48d0cb4aaa2fd3058e738ee5b2
Vartan Haghverdi <35344576+vhaghverdi@users.noreply.github.com>
2023-10-15 21:20:38 -0700

    able to parse map_regions.tga by itself if desired

--- README.md
+++ README.md
@@ -6,12 +6,16 @@ Please note that this script is not thoroughly tested and makes certain assumpti
 
 ## Usage
 
-Open a command prompt and run the following command:
+Open a command prompt and run one of the following commands:
 
-`python rtw-locate-settlements.py modpath`
+- To list settlement names, coordinates, and colors:
+  - `python rtw-locate-settlements.py mod_path`
+  - `mod_path` is the path to the mod folder itself, not `data` or any other subfolders.
+- To read map_regions.tga and list coordinates and colors only:
+  - `python rtw-locate-settlements.py map_regions_path`
+  - `map_regions_path` is the path to `map_regions.tga`
+  - in this mode, invalidly placed settlements will be listed in `invalid_settlements.csv`
 
-`modpath` is the path to the mod folder itself, not `data` or any other subfolders.
-
-The script will save settlement coordinates and names to `settlements.txt`.
+The script will save results to `settlements.csv`.
 
 Brought to you by the EB Online Team
--- rtw-locate-settlements.py
+++ rtw-locate-settlements.py
@@ -2,8 +2,11 @@
 
 """Saves a list of settlements and their coordinates.
 
-Usage:
-    python rtw-locate-settlements.py modpath
+To list settlement names and coordinates:
+    python rtw-locate-settlements.py mod_path
+
+To read map_regions.tga and list coordinates and colors only:
+    python rtw-locate-settlements.py map_regions_path
 
 MIT License
 
@@ -87,7 +90,7 @@ def parse_descr_regions(path_to_descr_regions: Path):
 def locate_settlements(path_to_map_regions: Path, path_to_descr_regions: Path):
     regions = parse_descr_regions(path_to_descr_regions)
     regions_by_color = {region["color"]: region["settlement"] for region in regions}
-    settlements = dict()
+    settlements = []
 
     with Image.open(path_to_map_regions) as im:
         for i, pixel in enumerate(im.getdata()):
@@ -114,36 +117,131 @@ def locate_settlements(path_to_map_regions: Path, path_to_descr_regions: Path):
                 color = list(color)[0]
                 name = regions_by_color[color]
                 y = im.height - y - 1
-                settlements[name] = (x, y)
+                settlements.append(
+                    {
+                        "settlement": name,
+                        "coordinate": (x, y),
+                        "color": color,
+                    }
+                )
 
     return settlements
 
 
+def is_settlement(pixel):
+    return pixel == (0, 0, 0)
+
+
+def is_port(pixel):
+    return pixel == (255, 255, 255)
+
+
+def is_sea(pixel):
+    r, g, _ = pixel
+    return r == 41 and g == 140
+
+
+def locate_settlements_map_only(path_to_map_regions):
+    settlements = []
+    invalid_settlements = []
+
+    with Image.open(path_to_map_regions) as im:
+        for i, pixel in enumerate(im.getdata()):
+            if is_settlement(pixel):
+                # pixel coordinates
+                x = i % im.width
+                y = i // im.width
+
+                # get adjacent pixels (not diagonally)
+                pixel_left = im.getpixel((x - 1, y)) if x > 0 else None
+                pixel_right = im.getpixel((x + 1, y)) if x < im.width - 1 else None
+                pixel_above = im.getpixel((x, y - 1)) if y > 0 else None
+                pixel_below = im.getpixel((x, y + 1)) if y < im.height - 1 else None
+                adjacent_pixels = [pixel_left, pixel_right, pixel_above, pixel_below]
+
+                # test if settlement is valid (at least one adjacent )
+                valid_region = False
+                for pixel in adjacent_pixels:
+                    if not (is_port(pixel) or is_sea(pixel)):
+                        valid_region = True
+                        region_color = pixel
+                        break
+
+                if valid_region:
+                    # invert y value to compute in-game coordinate
+                    y = im.height - y - 1
+                    settlements.append(
+                        {
+                            "coordinate": (x, y),
+                            "color": region_color,
+                        }
+                    )
+                else:
+                    invalid_settlements.append((x, y))
+
+    return settlements, invalid_settlements
+
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("Usage:\n\tpython rtw-locate-settlements.py modpath")
+        print("To list settlement names and coordinates:")
+        print("\tpython rtw-locate-settlements.py mod_path")
+        print("To read map_regions.tga and list coordinates and colors only:")
+        print("\tpython rtw-locate-settlements.py map_regions_path")
         exit()
 
     path_to_mod = Path(sys.argv[1])
-    path_to_base = path_to_mod / "data" / "world" / "maps" / "base"
-    path_to_map_regions = path_to_base / "map_regions.tga"
-    path_to_descr_regions = path_to_base / "descr_regions.txt"
+
+    # if user passed in map_regions.tga
+    if path_to_mod.is_file() and path_to_mod.name.lower() == "map_regions.tga":
+        settlements, invalid_settlements = locate_settlements_map_only(path_to_mod)
+        n = len(settlements)
+        if n == 0:
+            print("No settlements found.")
+        else:
+            f = open("settlements.csv", "w", encoding="utf-8")
+            f.write("x,y,r,g,b\n")
+            for settlement in settlements:
+                x, y = settlement["coordinate"]
+                r, g, b = settlement["color"]
+                f.write(f"{x},{y},{r},{g},{b}\n")
+            f.close()
+            print(f"{n} settlements saved to settlements.csv")
+        n = len(invalid_settlements)
+        if n > 0:
+            f = open("invalid_settlements.csv", "w", encoding="utf-8")
+            f.write("x,y\n")
+            for x, y in invalid_settlements:
+                f.write(f"{x},{y}\n")
+            f.close()
+            print(f"{n} invalid settlements saved to invalid_settlements.csv")
+        exit()
 
     if not path_to_mod.is_dir():
         print("Invalid mod directory.", file=sys.stderr)
         exit(1)
+
+    path_to_base = path_to_mod / "data" / "world" / "maps" / "base"
     if not path_to_base.is_dir():
         print("Missing data/world/maps/base directory.", file=sys.stderr)
         exit(1)
+
+    path_to_map_regions = path_to_base / "map_regions.tga"
     if not path_to_map_regions.is_file():
         print("Missing map_regions.tga.", file=sys.stderr)
         exit(1)
+
+    path_to_descr_regions = path_to_base / "descr_regions.txt"
     if not path_to_descr_regions.is_file():
         print("Missing descr_regions.txt.", file=sys.stderr)
         exit(1)
 
     settlements = locate_settlements(path_to_map_regions, path_to_descr_regions)
-    with open("settlements.txt", "w") as f:
-        for name, location in settlements.items():
-            x, y = location
-            f.write(f"{x:4}  {y:4}\t{name}\n")
+    with open("settlements.csv", "w", encoding="utf-8") as f:
+        f.write("settlement,x,y,r,g,b\n")
+        for settlement in settlements:
+            name = settlement["settlement"]
+            x, y = settlement["coordinate"]
+            r, g, b = settlement["color"]
+            f.write(f"{name},{x},{y},{r},{g},{b}\n")
+    print(f"{len(settlements)} settlements saved to settlements.csv")
